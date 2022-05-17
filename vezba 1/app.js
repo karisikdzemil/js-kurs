33b3a2b947bc70ef494eb05f3fe939f32866fadd
karisikdzemil <karisikdzemil@gmail.com>
2022-05-17 23:06:19 +0200

    vezbanje

---
+++ vezba 1/app.js
@@ -0,0 +1,74 @@
+
+// unos = Number(prompt("unesite broj redova"))
+// str = ""
+// for (i = 0; i < unos; i++) {
+//     str += "*"
+//     console.log(str)
+// }
+
+// unos = Number(prompt("unesite broj redova"))
+// str = ""
+// for (i = 0; i < unos; i++) {
+//     num = "1"
+//     str += "1"
+//     console.log(str)
+//     if(str++){
+//         unos + "1"
+//     }
+// }
+
+// x = Number(prompt("unesite broj redova"));
+// str = 1;
+// for (i = 0; i <= x; i++) {
+//     str++ += "0";
+//     console.log(str);
+//     console.log(str)
+//   }
+    // kolicina = Number(prompt("enter"));
+
+    // for (i = 1; i <=kolicina; i++) {
+    // str = "";
+    // for (j = 1; j <= i; j++) {
+
+    // }
+    // console.log(str);
+    // }
+
+// kolicina = Number(prompt("Enter your krajnja: "));
+
+// for (i = 1; i <= kolicina; i++) {
+//   str = "";
+//   for (j = 1; j <= i; j++) {
+//     str += i;
+//   }
+//   console.log(str);
+// }
+
+
+// unos = Number(prompt("unesi broj redova"))
+
+// for (i = 1; i <= unos; i++){
+//     str = "";
+//     for (j = 1; j <= i; j++){
+//         str += i;
+//         console.log(str)
+//     }
+// }
+
+// unos = Number(prompt("unesi broj"));
+
+// str ="";
+// for (i = 0; i < unos; i++){
+//     str += "*";
+//     console.log(str)
+// }
+
+unos = Number(prompt("unesi broj"));
+
+for (i = 1; i < unos; i++) {
+    str = "";
+    for (j = 1; j <= i; j++){
+        str += i;
+        console.log(str)
+    }
+}
